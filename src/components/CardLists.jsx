1ad49ac2da630f04a299835a8dd6df5cd9dc2557
Sanni-Singh <aryansingh7371@gmail.com>
2025-01-10 17:05:08 +0530

    heading chnges

--- src/components/CardLists.jsx
+++ src/components/CardLists.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 const Cardlists = ()=>{
     return(
         <div className="w-screen h-screen bg-[#333] ">
-            <p className="text-3xl text-white font-bold  text-center">You Can Put Your Manage Your Work Here...</p>
+            <p className="text-3xl text-white font-bold  text-center">You Can Put Your Work To Manage Here...</p>
             <div className="flex gap-8 justify-center pt-12">
             <Card titleList={"kamBakiHai"} title={"Pending Work"}/>
             <Card titleList={"kamChlRaha"} title={"In-Progress Work"}/>
