b2990edfd23a2c76d08f6dd97ab305765832b8ce
Sanni-Singh <aryansingh7371@gmail.com>
2025-01-10 16:32:52 +0530

    edited heading

--- src/components/CardLists.jsx
+++ src/components/CardLists.jsx
@@ -2,6 +2,7 @@ import Card from "./Card";
 const Cardlists = ()=>{
     return(
         <div className="w-screen h-screen bg-[#333] ">
+            <p className="text-3xl text-white font-bold  text-center">Kam Ka List Banane Wala Page</p>
             <div className="flex gap-8 justify-center pt-12">
             <Card titleList={"kamBakiHai"} title={"Kam Baki Hai"}/>
             <Card titleList={"kamChlRaha"} title={"Kam Chalu Hai"}/>
