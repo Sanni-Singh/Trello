b82e79031cc25b9b107410df4f84471c007cb80c
Sanni-Singh <aryansingh7371@gmail.com>
2025-01-10 17:03:42 +0530

    chngess

--- src/components/CardLists.jsx
+++ src/components/CardLists.jsx
@@ -2,11 +2,11 @@ import Card from "./Card";
 const Cardlists = ()=>{
     return(
         <div className="w-screen h-screen bg-[#333] ">
-            <p className="text-3xl text-white font-bold  text-center">Kam Ka List Banane Wala Page</p>
+            <p className="text-3xl text-white font-bold  text-center">You Can Put Your Manage Your Work Here...</p>
             <div className="flex gap-8 justify-center pt-12">
-            <Card titleList={"kamBakiHai"} title={"Kam Baki Hai"}/>
-            <Card titleList={"kamChlRaha"} title={"Kam Chalu Hai"}/>
-            <Card titleList={"kamKhtm"} title={"Kam Khtm Ho Gaya"}/>
+            <Card titleList={"kamBakiHai"} title={"Pending Work"}/>
+            <Card titleList={"kamChlRaha"} title={"In-Progress Work"}/>
+            <Card titleList={"kamKhtm"} title={"Completed Work"}/>
         </div>
         </div>
     )
